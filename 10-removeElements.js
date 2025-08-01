ac55f2aae7eae2b4abe8a6248e9702ce7c59092b
geekyneha <nehatualea@gmail.com>
2025-08-01 20:15:28 +0530

    Remove Duplicates

--- 10-removeElements.js
+++ 10-removeElements.js
@@ -2,7 +2,7 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 //non-decreasing order, in-place,
 
-const removeElements = (nums) => {
+const removeDuplicates = (nums) => {
   let x = 0;
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] > nums[x]) {
@@ -15,5 +15,5 @@ const removeElements = (nums) => {
 };
 
 let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
-console.log(removeElements(nums)); // Output: 5
+console.log(removeDuplicates(nums)); // Output: 5
 console.log(nums); // Output: [0,1,2,3,4,_,
